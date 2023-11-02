ce8c0baefeb9cc321d269613e7a3f52b43a6a3f1
LucasDepoot <lucas.depoot@oclock.school>
2023-11-03 00:19:39 +0100

    on change tout et on recommence

--- javascript/colorMode.js
+++ javascript/colorMode.js
@@ -1,10 +1,42 @@
 const colorButton = document.querySelector(".choiceColor");
-let color = "green";
-// idée generale -> changer la variable color en fonction du click ,modifié couleur ici et modifier nom de classe dans darkmode 
+
 colorButton.addEventListener("click",themeColor);
 
-
+// TODO Transformer cette page en themegreen et faire la meme chose pour les autres couleurs 
+// TODO Voir pour en faire des objets
 function themeColor(event){
-    const currentButton = event.target;
-    console.log(currentButton);
+    const titles =document.querySelectorAll("h1, h2, h3");
+    const colorTarget = event.target;
+    const cards = document.querySelectorAll(".card");
+    const cardLeft = document.querySelector(".leftUl");
+    const cursors = document.querySelectorAll(".cursor p");
+    for(const card of cards){
+        for (const title of titles){
+            for (const cursor of cursors){
+                if (colorTarget.classList.contains("green")){     
+                    card.classList.add("cardGreen");
+                    cardLeft.classList.add("cardGreen"); 
+                    title.classList.add("titleGreen");
+                    cursor.classList.add("titleGreen");
+
+                }else if(colorTarget.classList.contains("red")){
+                    card.classList.remove("cardGreen");
+                    cardLeft.classList.remove("cardGreen");
+                    title.classList.remove("titleGreen");
+                    cursor.classList.remove("titleGreen");
+
+                }else if(colorTarget.classList.contains("yellow")){
+                    card.classList.remove("cardGreen");
+                    cardLeft.classList.remove("cardGreen");
+                    title.classList.remove("titleGreen");
+                    cursor.classList.remove("titleGreen"); 
+                }else{
+                    card.classList.remove("cardGreen");
+                    cardLeft.classList.remove("cardGreen");
+                    title.classList.remove("titleGreen");
+                    cursor.classList.remove("titleGreen"); 
+                }
+            }
+        }
+    }
 }
--- javascript/darkMode.js
+++ javascript/darkMode.js
@@ -2,67 +2,24 @@ const darkModeButton = document.querySelector(".choiceMode");
 darkModeButton.addEventListener("click", darkMode);
 
 function darkMode(){
+
     
-    const logodarkModeButton = document.querySelector(".darkModeChoice i");
+    const logodarkMode = document.querySelector(".darkModeChoice i");
     const container = document.querySelector(".container");
-    const link = document.querySelectorAll("a");
-    const colorItemLight = document.querySelectorAll(".greenItemLight");
-    const colorItemDark = document.querySelectorAll(".greenItemDark");
-    const colorTextLight = document.querySelectorAll(".greenTextLight");
-    console.log()
-    const colorTextDark = document.querySelectorAll(".greenTextDark");
-    const colorBgLight = document.querySelectorAll(".greenBgLight");
-    console.log()
-    const colorBgDark = document.querySelectorAll(".greenBgDark");
-
-    if(logodarkModeButton.classList == "fa-solid fa-sun"){
-        //------------change logo of button "darkmode"---------\\
-        logodarkModeButton.classList.remove("fa-sun");
-        logodarkModeButton.classList.add("fa-moon");
-        //------------change background of container ---------\\
-        container.classList.add("dark");
-        for(item of link){
-            item.classList.add("dark");
-        }
-        //------------change Item ---------\\
-        for(item of colorItemLight ){
-            item.classList.remove(color+"ItemLight");
-            item.classList.add(color+"ItemDark");
-        }
-        //------------change Text ---------\\
-        for(item of colorTextLight ){
-            item.classList.remove(color+"TextLight");
-            item.classList.add(color+"TextDark");
-        }
-        //------------change Background cards ---------\\
-        for(item of colorBgLight ){
-            item.classList.remove(color+"BgLight");
-            item.classList.add(color+"BgDark");
-        }
-
-    }else{
-        logodarkModeButton.classList.remove("fa-moon");
-        logodarkModeButton.classList.add("fa-sun");
-
-        container.classList.remove("dark");
-        for(item of link){
-            item.classList.remove("dark");
-        }
-
-        for(item of colorItemDark ){
-            item.classList.remove(color+"ItemDark");
-            item.classList.add(color+"ItemLight");
-        }
-
-        for(item of colorTextDark ){
-            item.classList.remove(color+"TextDark");
-            item.classList.add(color+"TextLight");
-        }
-        for(item of colorBgDark ){
-            item.classList.remove(color+"BgDark");
-            item.classList.add(color+"BgLight");
-        }
+    const links = document.querySelectorAll("a");
+    const cursor =document.querySelector(".cursor");
+    
+    container.classList.toggle("dark");
+    cursor.classList.toggle("dark");
+    for(const link of links){
+        link.classList.toggle("darkText");
     };
-    // ----------------------------------------------------\\
-
+    
+    if(logodarkMode.classList.contains("fa-sun")){
+        logodarkMode.classList.remove("fa-sun");
+        logodarkMode.classList.add("fa-moon");
+    }else{
+        logodarkMode.classList.remove("fa-moon");
+        logodarkMode.classList.add("fa-sun");
+    }
 }
