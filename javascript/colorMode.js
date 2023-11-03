e2eba8c2b6f60b6764cd20ff68dc3ec42239c1d7
LucasDepoot <lucas.depoot@oclock.school>
2023-11-03 22:00:19 +0100

    design final

--- javascript/colorMode.js
+++ javascript/colorMode.js
@@ -1,42 +1,49 @@
-const colorButton = document.querySelector(".choiceColor");
-
-colorButton.addEventListener("click",themeColor);
-
-// TODO Transformer cette page en themegreen et faire la meme chose pour les autres couleurs 
-// TODO Voir pour en faire des objets
-function themeColor(event){
-    const titles =document.querySelectorAll("h1, h2, h3");
+const colorButton = document.querySelectorAll(".colored");
+for(const button of colorButton){
+button.addEventListener("click",themeBlue);}
+ let color = "green";
+function themeBlue(event){
+    let color = "green";
     const colorTarget = event.target;
-    const cards = document.querySelectorAll(".card");
-    const cardLeft = document.querySelector(".leftUl");
-    const cursors = document.querySelectorAll(".cursor p");
-    for(const card of cards){
-        for (const title of titles){
-            for (const cursor of cursors){
-                if (colorTarget.classList.contains("green")){     
-                    card.classList.add("cardGreen");
-                    cardLeft.classList.add("cardGreen"); 
-                    title.classList.add("titleGreen");
-                    cursor.classList.add("titleGreen");
-
-                }else if(colorTarget.classList.contains("red")){
-                    card.classList.remove("cardGreen");
-                    cardLeft.classList.remove("cardGreen");
-                    title.classList.remove("titleGreen");
-                    cursor.classList.remove("titleGreen");
-
-                }else if(colorTarget.classList.contains("yellow")){
-                    card.classList.remove("cardGreen");
-                    cardLeft.classList.remove("cardGreen");
-                    title.classList.remove("titleGreen");
-                    cursor.classList.remove("titleGreen"); 
-                }else{
-                    card.classList.remove("cardGreen");
-                    cardLeft.classList.remove("cardGreen");
-                    title.classList.remove("titleGreen");
-                    cursor.classList.remove("titleGreen"); 
-                }
-            }
-        }
+    if(colorTarget.classList.contains("blue")){
+        color = "blue";
+    }else if (colorTarget.classList.contains("red")){
+        color = "red";
+    }else if (colorTarget.classList.contains("yellow")){
+        color = "yellow";
     }
+
+    const container = document.querySelector(".container");
+    const cards = document.querySelectorAll(".card");
+    const nav = document.querySelector(".navBar");
+    const name = document.querySelector("h1");
+    const work = document.querySelector("h2");
+
+    console.log(color);
+    for (const card of cards) {
+        card.classList.remove("blueCard");
+        card.classList.remove("greenCard");
+        card.classList.remove("yellowCard");
+        card.classList.remove("redCard");
+        card.classList.add(color+"Card");
+
+    }
+
+    nav.classList.remove("blueNav");
+    nav.classList.remove("greenNav");
+    nav.classList.remove("yellowNav");
+    nav.classList.remove("redNav");
+    nav.classList.add(color+"Nav");
+
+    name.classList.remove("blueH1");
+    name.classList.remove("greenH1");
+    name.classList.remove("yellowH1");
+    name.classList.remove("redH1");
+    name.classList.add(color+"H1");
+
+    work.classList.remove("blueH2");
+    work.classList.remove("greenH2");
+    work.classList.remove("yellowH2");
+    work.classList.remove("redH2");
+    work.classList.add(color+"H2");
 }
